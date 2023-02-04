df28a7c8049b63c37b6426d255d7b7101bae9daf
shrief hesham <62178884+shriefhesham@users.noreply.github.com>
2023-02-04 02:49:49 +0200

    Add files via upload

---
+++ C#/Day01/ConsoleApp1/Day02/D02_Taks/Arr_Reverse_Task/Program.cs
@@ -0,0 +1,5 @@
+﻿string Word = Console.ReadLine() ;
+string[] ReversedString = Word.Split(' ') ;
+Array.Reverse(ReversedString) ;
+string Word1 = string.Join(" ", ReversedString) ;    
+Console.WriteLine(Word1) ;    
