f7797b8d9885a3d601898c04869fec7ea70cc798
shrief hesham <62178884+shriefhesham@users.noreply.github.com>
2023-02-06 17:35:00 +0200

    Day4

---
+++ C#/Day01/ConsoleApp1/Day04/Employee/Employee_Task/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+
+namespace Struct_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hiredate Myhire = new Hiredate();
+            Employee[] employees = new Employee[3];
+           
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine($"Enter Data For Employee Number  {i + 1}");
+                Console.WriteLine(" Enter Employee id Number ");
+                int id = int.Parse(Console.ReadLine());
+                employees[i].ID = id;
+
+                Console.WriteLine("Enter Your Gender");
+                string gender =Console.ReadLine();
+                employees[i].Gender = (Gender)Enum.Parse(typeof(Gender),gender);
+
+                Console.WriteLine("Enter Employee salary Number");
+                int salary = int.Parse(Console.ReadLine());
+                employees[i].Salary= salary;
+
+                Console.WriteLine("Enter Employee Scurity Number");
+                string ScurityL = Console.ReadLine();
+                employees[i].ScurityLevel = (ScurityLevel)Enum.Parse(typeof(ScurityLevel), ScurityL);
+
+
+                Console.WriteLine("Enter your day");
+                int day = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your month");
+                int month = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your year");
+                int year = int.Parse(Console.ReadLine());
+
+                Myhire = new Hiredate(day, month, year);
+                employees[i].Hiredate= Myhire;
+            }
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine($" Employee Number {i+1} is ");
+                Console.WriteLine($" EmployeeID          ----> {employees[i].ID}");
+                Console.WriteLine($" Employee_Salary     ----> {employees[i].Salary}");
+                Console.WriteLine($" EmployeeHireDate    ----> {employees[i].Hiredate}");
+                Console.WriteLine($" Employee_Gender     ----> {employees[i].Hiredate}");
+                Console.WriteLine($" Employee_Position   ----> {employees[i].ScurityLevel}");
+                Console.WriteLine("-----------------------------------------------------------------------"); 
+            }
+        }
+    }
+}
---
+++ C#/Day01/ConsoleApp1/Day04/Struct_Task/Struct_Task/EmployeeSearch.cs
@@ -0,0 +1,85 @@
+﻿using Elasticsearch.Net;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Struct_Task
+{
+    internal class EmployeeSearch
+    {
+       public int[] nationalIds;
+       public Employee[] employees;
+       int size;
+
+
+        public EmployeeSearch(int _size)
+        {
+            size= _size;
+            this.nationalIds = new int[size];
+            this.employees = new Employee[size];
+        }
+
+
+
+        public int[] NationalId
+        {
+            set { nationalIds = value; }
+            get { return nationalIds; }
+        }
+
+        public Employee[] Employees
+        {
+            set { employees = value; }
+            get { return employees; }
+        }
+
+        public string this[int _nationalid]
+        {
+            get
+            {
+                for (int i = 0; i < employees.Length; i++)
+                {
+                    if (employees[i].ID == _nationalid)
+                        return employees[i].ToString();
+                }
+                return null;
+            }
+        }
+
+        public string this[int day , int month , int year]
+        {
+            get
+            {
+                for (int i = 0; i < employees.Length; i++)
+                {
+                    if (employees[i].Hiredate.year == year && employees[i].Hiredate.month == month && employees[i].Hiredate.day == day)
+                        return employees[i].ToString();
+                }
+                return null;
+            }
+        }
+
+
+        public string this[string _Name]
+        {
+            get
+            {
+                for (int i = 0; i < employees.Length; i++)
+                {
+                    if (employees[i].Name == _Name)
+                        return employees[i].ToString();
+                }
+                return null;
+            }
+        }
+
+
+
+    }
+
+    
+}
