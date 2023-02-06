cff88540744aa1517350dee164c902c3aae825a1
shrief hesham <62178884+shriefhesham@users.noreply.github.com>
2023-02-06 17:36:31 +0200

    Delete C#/Day01/ConsoleApp1 directory

--- C#/Day01/ConsoleApp1/Day02/D02_Taks/Arr_Reverse_Task/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿string Word = Console.ReadLine() ;
-string[] ReversedString = Word.Split(' ') ;
-Array.Reverse(ReversedString) ;
-string Word1 = string.Join(" ", ReversedString) ;    
-Console.WriteLine(Word1) ;    
