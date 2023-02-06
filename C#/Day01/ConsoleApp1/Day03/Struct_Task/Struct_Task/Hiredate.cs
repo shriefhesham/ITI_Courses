5aeaea394c454e6a4bf6f862f0de9a9b819809d6
shrief hesham <62178884+shriefhesham@users.noreply.github.com>
2023-02-06 15:40:44 +0200

    Add files via upload

---
+++ C#/Day01/ConsoleApp1/Day03/Struct_Task/Struct_Task/Hiredate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Task
+{
+    internal struct Hiredate
+    {
+        internal int day;
+        internal int month;
+        internal int year;
+
+        public Hiredate(int _day , int _month , int _year  )
+        {
+            day = _day;
+            month = _month;
+            year = _year;
+        }
+
+        public override string ToString()
+        {
+            return $"{day}/{month}/{year}";
+        }
+
+    }
+}
---
+++ C#/Day01/ConsoleApp1/Day03/Struct_Task/Struct_Task/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+
+namespace Struct_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hiredate Myhire = new Hiredate();
+            Employee[] employees = new Employee[3];
+           
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine($"Enter Data For Employee Number  {i + 1}");
+                Console.WriteLine(" Enter Employee id Number ");
+                int id = int.Parse(Console.ReadLine());
+                employees[i].SetId(id);
+
+                Console.WriteLine("Enter Your Gender");
+                string gender = Console.ReadLine();
+                employees[i].SetGender(gender);
+
+                Console.WriteLine("Enter Employee salary Number");
+                int salary = int.Parse(Console.ReadLine());
+                employees[i].setSalary(salary);
+
+                Console.WriteLine("Enter Employee Scurity Number");
+                int ScurityL = int.Parse(Console.ReadLine());
+                employees[i].setScurityLevel(ScurityL);
+
+                Console.WriteLine("Enter your day");
+                int day = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your month");
+                int month = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your year");
+                int year = int.Parse(Console.ReadLine());
+
+                Myhire = new Hiredate(day, month, year);
+                employees[i].SetHireDate(Myhire);
+            }
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine("-----------------------------------------------------------------------");
+                    Console.WriteLine($" Employee Number {i+1} is ");
+                    Console.WriteLine($" EmployeeID          ----> {employees[i].getId()}");
+                    Console.WriteLine($" Employee_Salary     ----> {employees[i].getSalary()}");
+                    Console.WriteLine($" EmployeeHireDate    ----> {employees[i].getHireDate()}");
+                    Console.WriteLine($" Employee_Gender     ----> {employees[i].getGender()}"); 
+                    Console.WriteLine($" Employee_Position   ----> {employees[i].getScurityLevel()}"); 
+                Console.WriteLine("-----------------------------------------------------------------------"); 
+            }
+        }
+    }
+}
