1d57783953dd7763f381c64bb83b3193860ca82e
shrief hesham <62178884+shriefhesham@users.noreply.github.com>
2023-02-07 19:38:35 +0200

    Add files via upload

---
+++ C#/Day02/D02_Taks/One's_Task/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Diagnostics;
+
+void numofones_1()
+{
+    int count = 0;
+    var stopwatch = new System.Diagnostics.Stopwatch();
+    stopwatch.Start();
+    for (int i = 1; i <= 99_999_999; i++)
+    {
+        int j = i;
+        while (j != 0)
+        {
+            if (j % 10 == 1)
+                count++ ;
+            j /= 10 ;
+        }
+    }
+    stopwatch.Stop();
+    Console.WriteLine($"Number of One's by Numaric way : {count} , Time : {stopwatch.Elapsed}") ;
+}
+
+void numOfOnes_2()
+{
+    int count = 0;
+    string one = "";
+    var stopwatch = new System.Diagnostics.Stopwatch();
+    stopwatch.Start();
+    for (int i = 0; i < 99999999; i++)
+    {
+        one = Convert.ToString(i);
+        foreach (char j in one)
+        {
+            if (j == '1')
+            {
+                count++;
+            }
+        }
+    }
+    stopwatch.Stop();
+    Console.WriteLine($"Number of One's by String way : {count} Time : {stopwatch.Elapsed}" );
+}
+
+void numOfOnes_3()
+{
+    var stopwatch = new System.Diagnostics.Stopwatch();
+    stopwatch.Start();
+    int n = 8, counter = 0;
+    counter = (int)(n * Math.Pow(10, n - 1));
+    stopwatch.Stop();
+    Console.WriteLine($"Number of One's by Math way : {counter} Time : {stopwatch.Elapsed}");
+}
+
+
+//numofones_1();
+numOfOnes_2();
+//numOfOnes_3();
+
