d2db9f39a26753347a66e9ad1a476d344215366a
shrief hesham <62178884+shriefhesham@users.noreply.github.com>
2023-02-17 22:20:21 +0200

    Add files via upload

---
+++ C/Day1/Task3/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //convert from char to ASCII
+
+    char ch ;
+
+    printf(" please enter a character : ");
+
+    scanf("%c",&ch) ;
+
+    printf(" ASCII : %d\n",ch) ;
+
+    return 0 ;
+}
